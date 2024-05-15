4916f6a1310593bc629b7712f970eea6ea26d4cf
Paola Taboada <taboadapaola3@gmail.com>
2024-05-15 15:29:59 -0500

    update header for desktop version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,6 +8,36 @@ const Header = () => {
 			<Link href={"/"} className={styles.header__btn__primary}>
 				Online Portafolio
 			</Link>
+			<>
+				<div className={styles.header__separator} />
+				<Link href={"/"} className={styles.header__btn__secondary}>
+					Sobre mí
+				</Link>
+				<Link href={"/"} className={styles.header__btn__secondary}>
+					Educación
+				</Link>
+				<Link href={"/"} className={styles.header__btn__secondary}>
+					Experiencia
+				</Link>
+				<Link href={"/"} className={styles.header__btn__secondary}>
+					Proyectos
+				</Link>
+				<Link href={"/"} className={styles.header__btn__secondary}>
+					Contacto
+				</Link>
+
+				<>
+					<hr className={styles.header__line} />
+					<Image
+						src="/circle__footer.svg"
+						alt="Círculo"
+						width={10}
+						height={10}
+						priority
+						className={styles.header__circle}
+					/>
+				</>
+			</>
 			<button className={styles.header__btn__menu}>
 				<Image
 					src="/menu__icon.svg"
