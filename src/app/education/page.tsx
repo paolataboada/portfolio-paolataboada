34ddc3b31fe1255dbca56938c0eda2f3a6d89e07
Paola Taboada <taboadapaola3@gmail.com>
2024-05-16 16:33:06 -0500

    add education page in mobile and desktop version

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,5 +1,45 @@
+import styles from "./Education.module.css";
+
 const Education = () => {
-	return <div>Education</div>;
+	return (
+		<main className={styles.main}>
+			<div className={styles.main__wrapper}>
+				<div className={styles.main__title_section}>
+					<p>EDUCACIÓN</p>
+					<span></span>
+				</div>
+				<div className={styles.main__content_section}>
+					En 2020, durante mi último año de estudios en Administración
+					de Empresas, experimenté un cambio profundo en mi visión
+					profesional. La pandemia de COVID-19 fue un impulso para
+					descubrir nuevas oportunidades y desarrollar un apasionado
+					interés por la tecnología. Desde entonces, he enfocado mi
+					desarrollo profesional en explorar cómo la tecnología puede
+					revolucionar y mejorar múltiples aspectos del ámbito
+					empresarial y más allá.
+				</div>
+			</div>
+			<div className={styles.main__wrapper_education}>
+				<div className={styles.main__education_period}>2016 - 2021</div>
+				<div className={styles.main__educational_institute}>
+					<p>Laboratoria Bootcamp</p>
+					<p>Especialización en Desarrollo Web Front End | 2021</p>
+				</div>
+				<div className={styles.main__educational_institute}>
+					<p>Universidad Nacional Jorge Basadre Grohmann</p>
+					<p>Bachiller en Ciencias Administrativas | 2016 - 2020</p>
+				</div>
+				<div className={styles.main__educational_institute}>
+					<p>Instituto de Informática y Telecomunicaciones</p>
+					<p>Técnico En Ofimática | 2019</p>
+				</div>
+				<div className={styles.main__educational_institute}>
+					<p>Centro de Idiomas - UNJBG</p>
+					<p>English | Advanced Level | 2016 - 2018</p>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Education;
