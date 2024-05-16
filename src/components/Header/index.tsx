4dc9fef1674a96d430bec1bceba21f5dd9102c0b
Paola Taboada <taboadapaola3@gmail.com>
2024-05-16 12:27:02 -0500

    add routes and active navigation, add favicon, update responsive menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,74 @@
+"use client";
 import Image from "next/image";
 import styles from "./Header.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+	const pathname = usePathname();
+	console.log(pathname);
+
 	return (
 		<div className={styles.header__wrapper}>
 			<Link href={"/"} className={styles.header__btn__primary}>
 				Online Portafolio
 			</Link>
-			<>
-				<div className={styles.header__separator} />
-				<Link href={"/"} className={styles.header__btn__secondary}>
-					Sobre mí
-				</Link>
-				<Link href={"/"} className={styles.header__btn__secondary}>
-					Educación
-				</Link>
-				<Link href={"/"} className={styles.header__btn__secondary}>
-					Experiencia
-				</Link>
-				<Link href={"/"} className={styles.header__btn__secondary}>
-					Proyectos
-				</Link>
-				<Link href={"/"} className={styles.header__btn__secondary}>
-					Contacto
-				</Link>
+			<div className={styles.header__separator} />
+			<Link
+				href={"/about-me"}
+				className={`${styles.header__btn__secondary} ${
+					pathname === "/about-me" ? `${styles.active}` : ""
+				}`}
+			>
+				Sobre mí
+			</Link>
+			<Link
+				href={"/education"}
+				className={`${styles.header__btn__secondary} ${
+					pathname === "/education" ? `${styles.active}` : ""
+				}`}
+			>
+				Educación
+			</Link>
+			<Link
+				href={"/experience"}
+				className={`${styles.header__btn__secondary} ${
+					pathname === "/experience" ? `${styles.active}` : ""
+				}`}
+			>
+				Experiencia
+			</Link>
+			<Link
+				href={"/projects"}
+				className={`${styles.header__btn__secondary} ${
+					pathname === "/projects" ? `${styles.active}` : ""
+				}`}
+			>
+				Proyectos
+			</Link>
+			<Link
+				href={"/contact"}
+				className={`${styles.header__btn__secondary} ${
+					pathname === "/contact" ? `${styles.active}` : ""
+				}`}
+			>
+				Contacto
+			</Link>
 
-				<>
-					<hr className={styles.header__line} />
-					<Image
-						src="/circle__footer.svg"
-						alt="Círculo"
-						width={10}
-						height={10}
-						priority
-						className={styles.header__circle}
-					/>
-				</>
+			<>
+				<hr className={styles.header__line} />
+				<Image
+					src="/circle__footer.svg"
+					alt="Círculo"
+					width={10}
+					height={10}
+					priority
+					className={styles.header__circle}
+				/>
 			</>
-			<button className={styles.header__btn__menu}>
+
+			{/* MENU RESPONSIVE */}
+			<label htmlFor="menu" className={styles.header__btn__menu}>
 				<Image
 					src="/menu__icon.svg"
 					alt="Menu hamburguesa"
@@ -46,7 +76,15 @@ const Header = () => {
 					height={35}
 					priority
 				/>
-			</button>
+			</label>
+			<input type="checkbox" id="menu" className={styles.header__input} />
+			<div className={styles.menu__responsive}>
+				<a href={"/about-me"}>Sobre mí</a>
+				<a href={"/education"}>Educación</a>
+				<a href={"/experience"}>Experiencia</a>
+				<a href={"/projects"}>Proyectos</a>
+				<a href={"/contact"}>Contacto</a>
+			</div>
 		</div>
 	);
 };
